049ce191df3161c7815cedde783b4c93abbc7565
kglowprofile <136592321+kahgin@users.noreply.github.com>
2025-01-08 11:48:51 +0800

    created data

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,43 @@
 module Main where
 
+data Subject = Subject { 
+    -- must have
+    subjectName :: String, 
+    color :: String,
+
+    -- optional
+    description :: Maybe String,
+    lessons :: [Lesson]
+
+    } deriving (Show)
+
+data Lesson = Lesson {
+    -- must have
+    time :: Maybe String,
+
+    -- optional
+    room :: String,
+    teacher :: String
+
+    } deriving (Show)
+
+data Timetable = Timetable {
+    -- must have
+    timetableName :: String,
+    palette :: [String],
+    start :: String,
+    end :: String,
+
+    -- optional
+    subjects :: [Subject]
+
+    } deriving (Show)
+
+data Palette = Palette {
+    paletteName :: String,
+    colors :: [String]
+
+    } deriving (Show)
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = putStrLn "Hello, world!"
