ca8fe808249c250b27f0ed9a63b6159ae36d0034
kglowprofile <136592321+kahgin@users.noreply.github.com>
2025-01-10 00:17:26 +0800

    create Timetable.hs

--- app/File.hs
+++ app/File.hs
@@ -1,17 +1,19 @@
+
 module File where
 
+import Timetable
 import Data.Aeson (encode, decode)
 import qualified Data.ByteString.Lazy as B
-import System.IO (writeFile, readFile)
-
+{-
 -- Save timetable to file
 saveTimetable :: Timetable -> IO ()
 saveTimetable timetable = do
     let json = encode timetable
-    writeFile "timetable.json" json
+    B.writeFile "timetable.json" json
 
 -- Load timetable from file
 loadTimetable :: IO (Maybe Timetable)
 loadTimetable = do
-    json <- readFile "timetable.json"
+    json <- B.readFile "timetable.json"
     return $ decode json
+-}
---
+++ app/Timetable.hs
@@ -0,0 +1,140 @@
+module Timetable where
+
+import UI
+import Helper
+import System.IO
+import Data.Time
+import Data.Aeson
+import GHC.Generics
+import Control.Monad
+
+data Timetable = Timetable {
+    timetableName :: String,
+    subjects :: [Subject]
+    } deriving (Show)
+
+data Subject = Subject { 
+    subjectName :: String,
+    lessons :: [Lesson]
+    } deriving (Show)
+
+data Lesson = Lesson {
+    day :: DayOfWeek,
+    time :: TimeRange,
+    room :: String,
+    lecturer :: String
+    } deriving (Show)
+
+createTimetable :: IO Timetable
+createTimetable = do
+    clearScreen
+    printHeader "Create a new timetable"
+    putStr "Timetable name: "
+    hFlush stdout
+    timetableName <- getLine
+
+    let newTimetable = Timetable { timetableName = timetableName, subjects = [] }
+    manageTimetable newTimetable
+
+
+manageTimetable :: Timetable -> IO Timetable
+manageTimetable timetable = do
+    clearScreen
+    printHeader (timetableName timetable)
+    putStrLn "1. Add a subject"
+    putStrLn "2. Edit a subject"
+    putStrLn "3. Delete a subject"
+    printExit
+
+    putStr "Select option (1-3): "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> createSubject                        >> manageTimetable timetable
+        "2" -> putStrLn "editSubject"               >> manageTimetable timetable
+        "3" -> putStrLn "deleteSubject"             >> manageTimetable timetable
+        ""  -> return timetable
+        _   -> printError "\nInvalid choice!\n\n"   >> manageTimetable timetable
+
+editTimetable :: IO ()
+editTimetable = do
+    clearScreen
+    printHeader "Edit a timetable"
+
+createSubject :: IO Subject
+createSubject = do
+    clearScreen
+    printHeader "Add a subject"
+    putStr "Subject name: "
+    hFlush stdout
+    subjectName <- getLine
+
+    let newSubject = Subject { subjectName = subjectName, lessons = [] }
+    manageSubject newSubject
+      
+manageSubject :: Subject -> IO Subject
+manageSubject subject = do
+    clearScreen
+    printHeader (subjectName subject)
+    unless (null $ lessons subject) $ displayLessons (lessons subject)
+    putStrLn "1. Add a lesson"
+    putStrLn "2. Edit a lesson"
+    putStrLn "3. Delete a lesson"
+    printExit
+
+    putStr "Select option (1-3): "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> do
+            newLesson <- createLesson
+            let updatedSubject = subject { lessons = lessons subject ++ [newLesson] }
+            manageSubject updatedSubject
+        "2" -> putStrLn "editLesson"                >> manageSubject subject
+        "3" -> displayLessons (lessons subject)     >> manageSubject subject
+        ""  -> return subject
+        _   -> printError "\nInvalid choice!\n\n"   >> manageSubject subject
+
+displayLessons :: [Lesson] -> IO ()
+displayLessons lessons = do
+    let border = replicate 30 '='
+    putStrLn border
+    mapM_ printLessonWithBorder (zip lessons [1..])
+    putStrLn border
+    where
+        printLessonWithBorder (lesson, index) = do
+            putStrLn $ "Day: " ++ show (day lesson)
+            putStrLn $ "Time: " ++ show (time lesson)
+            putStrLn $ "Room: " ++ room lesson
+            putStrLn $ "Lecturer: " ++ lecturer lesson
+            if index == length lessons then return () else putStrLn $ replicate 30 '-'
+
+editSubject :: IO ()
+editSubject = do
+    putStr "Select option: "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> putStrLn "editSubjectName"         >> editSubject
+        "2" -> putStrLn "editLesson"              >> editSubject
+        ""  -> return ()
+        _   -> printError "\nInvalid choice!\n\n" >> editSubject
+
+createLesson :: IO Lesson
+createLesson = do
+    clearScreen
+    printHeader "Add a lesson"
+
+    day <- validateDay
+
+    time <- validateTime
+
+    putStr "Room (optional): "
+    hFlush stdout
+    room <- getLine
+
+    putStr "Lecturer (optional): "
+    hFlush stdout
+    lecturer <- getLine
+
+    return Lesson { day = day, time = time, room = room, lecturer = lecturer }
