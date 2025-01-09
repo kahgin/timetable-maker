862b6995e2900f59b24cdcffb2ef2d46235b9856
kglowprofile <136592321+kahgin@users.noreply.github.com>
2025-01-09 13:25:23 +0800

    add menus

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,18 @@ module Main where
 import UI
 import System.IO
 
+data Timetable = Timetable {
+    -- must have
+    timetableName :: String,
+    palette :: [String],
+    start :: String,
+    end :: String,
+
+    -- optional
+    subjects :: [Subject]
+
+    } deriving (Show)
+
 data Subject = Subject { 
     -- must have
     subjectName :: String, 
@@ -25,36 +37,142 @@ data Lesson = Lesson {
 
     } deriving (Show)
 
-data Timetable = Timetable {
-    -- must have
-    timetableName :: String,
-    palette :: [String],
-    start :: String,
-    end :: String,
+createTimetable :: IO Timetable
+createTimetable = do
+    clearScreen
+    printHeader "Create a new timetable"
+    putStr "Timetable name: "
+    hFlush stdout
+    timetableName <- getLine
 
-    -- optional
-    subjects :: [Subject]
+    let newTimetable = Timetable { timetableName = timetableName, palette = [], start = "", end = "", subjects = [] }
+    manageTimetable newTimetable
 
-    } deriving (Show)
+manageTimetable :: Timetable -> IO Timetable
+manageTimetable timetable = do
+    clearScreen
+    printHeader (timetableName timetable)
+    putStrLn "1. Add a subject"
+    putStrLn "2. Edit a subject"
+    putStrLn "3. Display all subjects"
+    putStrLn "4. Finish"
 
-data Palette = Palette {
-    paletteName :: String,
-    colors :: [String]
+    putStr "Select option (1-4): "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> createSubject                        >> manageTimetable timetable
+        "2" -> putStrLn "editSubject"               >> manageTimetable timetable
+        "3" -> putStrLn "displaySubjects"           >> manageTimetable timetable
+        "4" -> return timetable
+        _   -> printError "\nInvalid choice!\n\n"   >> manageTimetable timetable
 
-    } deriving (Show)
+createSubject :: IO Subject
+createSubject = do
+    clearScreen
+    printHeader "Add a subject"
+    putStr "Subject name: "
+    hFlush stdout
+    subjectName <- getLine
 
-main :: IO ()
-main = do
-    printMenu
+    let newSubject = Subject { subjectName = subjectName, color = "", description = Nothing, lessons = [] }
+    manageSubject newSubject
+      
+manageSubject :: Subject -> IO Subject
+manageSubject subject = do
+    clearScreen
+    printHeader (subjectName subject)
+    if not (null (lessons subject))
+        then displayLessons (lessons subject)
+        else return ()
+    putStrLn "1. Add a lesson"
+    putStrLn "2. Edit a lesson"
+    putStrLn "3. Delete a lesson"
+    putStrLn "4. Finish"
+
+    putStr "Select option (1-4): "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> do
+            newLesson <- createLesson
+            let updatedSubject = subject { lessons = lessons subject ++ [newLesson] }
+            manageSubject updatedSubject
+        "2" -> putStrLn "editLesson"                >> manageSubject subject
+        "3" -> displayLessons (lessons subject)     >> manageSubject subject
+        "4" -> return subject
+        _   -> printError "\nInvalid choice!\n\n"   >> manageSubject subject
+
+displayLessons :: [Lesson] -> IO ()
+displayLessons lessons = do
+    let border = replicate 40 '='
+    putStrLn border
+    mapM_ printLesson lessons
+    putStrLn border
+  where
+    printLesson lesson = do
+        putStrLn $ "Day: " ++ day lesson
+        putStrLn $ "Time: " ++ time lesson
+        putStrLn $ "Room: " ++ room lesson
+        putStrLn $ "Teacher: " ++ teacher lesson
+        putStrLn $ replicate 40 '-'
+
+{-editSubject :: IO ()
+editSubject = do
+    for i, subject in subjects do
+        putStrLn (i. <> subject.subjectName)
+    putStr "Select option: "
+    hFlush stdout
+    choice <- getLine
+    case choice of
+        "1" -> putStrLn "editSubjectName"
+        "2" -> putStrLn "editSubjectColor"
+        "3" -> putStrLn "editSubjectDescription"
+        "4" -> putStrLn "finish"
+        _   -> printError "\nInvalid choice!\n\n" >> editSubject-}
+
+createLesson :: IO Lesson
+createLesson = do
+    clearScreen
+    printHeader "Add a lesson"
+    putStr "Day: "
+    hFlush stdout
+    day <- getLine
+
+    putStr "Time: "
+    hFlush stdout
+    time <- getLine
+
+    putStr "Room (optional): "
+    hFlush stdout
+    room <- getLine
+
+    putStr "Teacher (optional): "
+    hFlush stdout
+    teacher <- getLine
+
+    return Lesson { day = day, time = time, room = room, teacher = teacher }
+
+mainMenu :: IO ()
+mainMenu = do
+    -- clearScreen
+    printHeader "Welcome to Timetable Creator!"
+    putStrLn "1. Create a new timetable"
+    putStrLn "2. Edit a timetable"
+    putStrLn "3. Import a timetable"
+    putStrLn "4. Export a timetable"
+    putStrLn "Press [Enter] to Exit\n"
 
     putStr "Select option (1-5): "
     hFlush stdout
     choice <- getLine
     case choice of
-        "1" -> putStrLn "createTimetable"
+        "1" -> createTimetable                      >> mainMenu
         "2" -> putStrLn "editTimetable"
         "3" -> putStrLn "importTimetable"
         "4" -> putStrLn "exportTimetable"
-        "5" -> putStrLn "Goodbye!"
-        _   -> printError "\nInvalid choice!\n\n" >> main
-    
+        "" -> putStrLn "Goodbye!"
+        _   -> printError "\nInvalid choice!\n\n"   >> mainMenu
+
+main :: IO ()
+main = mainMenu
--- app/UI.hs
+++ app/UI.hs
@@ -2,6 +2,14 @@ module UI where
 
 import System.Console.ANSI
 
+palettes :: [(String, [String])]
+palettes =
+    [ ("Pastel", ["#e4dde3", "#ffe8d6", "#fcd9db", "#dae7e3", "#bee1e6", "#c6def2", "#cddafd"])
+    , ("French Tapestry", ["#a3886b", "#dead9c", "#ebdac6", "#9fb6ae", "#fedcaf"])
+    , ("Earthy Rainbow", ["#dfd1e0", "#bbdee4", "#c1dad7", "#edddc6", "#dead9c"])
+    , ("Wildflower", ["#d4ace0", "#d78393", "#f2aead", "#f7e9de", "#cf89ae"])
+    ]
+
 printHeader :: String -> IO ()
 printHeader header = do
     let lineLength = length header + 8
@@ -13,17 +21,17 @@ printHeader header = do
     putStrLn line
     setSGR [Reset]
 
-printMenu :: IO ()
-printMenu = do
-    printHeader "Welcome to Timetable Creator!"
-    putStrLn "1. Create a new timetable"
-    putStrLn "2. Edit a timetable"
-    putStrLn "3. Import a timetable"
-    putStrLn "4. Export a timetable"
-    putStrLn "5. Exit"
-
 printError :: String -> IO()
 printError message = do
     setSGR [SetColor Foreground Dull Red]
     putStr message
+    setSGR [Reset]
+
+clearScreen :: IO ()
+clearScreen = putStr "\ESC[2J"
+
+printUnavailable :: String -> IO ()
+printUnavailable message = do
+    setSGR [SetColor Foreground Vivid Black]
+    putStrLn message
     setSGR [Reset]
